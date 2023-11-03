dfce9b6ffbc16bfdd313ba1686ccebafe42d1e4f
shanaezak <149697420+shanaezak@users.noreply.github.com>
2023-11-03 19:40:17 +0530

    Update autocomplete.js

--- apps/block_scout_web/assets/js/lib/autocomplete.js
+++ apps/block_scout_web/assets/js/lib/autocomplete.js
@@ -6,7 +6,7 @@ import { appendTokenIcon } from './token_icon'
 import { escapeHtml } from './utils'
 import xss from 'xss'
 
-const placeHolder = 'Search by address, token symbol, name, transaction hash, or block number'
+const placeHolder = 'Search by address, token symbol, name, transaction hash'
 const dataSrc = async (query, id) => {
   try {
     // Loading placeholder text
